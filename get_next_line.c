9b27824826e81afe2fdf9ed605a9a0a0ceff6dab
yaequb <153851249+yaequb@users.noreply.github.com>
2024-02-25 18:11:24 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -12,7 +12,8 @@
 
 #include "get_next_line.h"
 #include <unistd.h>
-#include <stdio.h>
+// #include <stdio.h>
+// #include <fcntl.h>
 
 char	*ft_read_str(int fd, char *this_str)
 {
@@ -32,8 +33,6 @@ char	*ft_read_str(int fd, char *this_str)
 			return (NULL);
 		}
 		buffer[bts] = '\0';
-		printf("this_str: (%s)\n", this_str);
-		printf("buffer: (%s)\n",buffer);
 		this_str = ft_strjoin(this_str, buffer);
 	}
 	free(buffer);
@@ -48,42 +47,28 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	this_str = ft_read_str(fd, this_str);
-	printf("read_str: (%s)\n",this_str);
 	if (!this_str)
 		return (NULL);
 	line = ft_get_line(this_str);
-	printf("get_line: (%s)\n", line);
 	this_str = ft_next_line(this_str);
 	return (line);
 }
 
-#include <fcntl.h>
-int	main(void)
-{
-	char	*line;
-	int		i;
-	int		fd1;
-	int		fd2;
-	int		fd3;
-	fd1 = open("test/test1.txt", O_RDONLY);
-	fd2 = open("test/test2.txt", O_RDONLY);
-	fd3 = open("test/test3.txt", O_RDONLY);
-	i = 1;
-	while (i < 7)
-	{
-		// line = get_next_line(fd1);
-		// printf("line [%02d]: %s\n", i, line);
-		// free(line);
-		line = get_next_line(fd2);
-		// printf("line [%02d]: %s\n", i, line);
-		free(line);
-		// line = get_next_line(fd3);
-		// printf("line [%02d]: %s\n", i, line);
-		// free(line);
-		i++;
-	}
-	close(fd1);
-	close(fd2);
-	close(fd3);
-	return (0);
-}
+// int	main(void)
+// {
+// 	int		i;
+// 	int		fd;
+// 	char	*line;
+
+// 	fd = open("./test1.txt", O_RDONLY);
+// 	i = 1;
+// 	while (i < 6)
+// 	{
+// 		line = get_next_line(fd);
+// 		printf("line %d: (%s)\n", i, line);
+// 		free(line);
+// 		i++;
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
